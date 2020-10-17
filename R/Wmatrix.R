c7f3bf6abcf42218c16a67ed00ee5de284e7995e
Germano Costa Neto <germano.cneto@gmail.com>
2020-10-17 13:34:06 -0300

    Update Wmatrix.R

--- R/Wmatrix.R
+++ R/Wmatrix.R
@@ -32,46 +32,46 @@
 #' env.data = get_weather(lat = -13.05, lon = -56.05, country = 'BRA')
 #'
 #' ### Mean-centered and scaled matrix
-#' W <- W.matrix(env.data = env.data, by.interval = FALSE)
+#' W <- W_matrix(env.data = env.data, by.interval = FALSE)
 #'
 #' ### Same as SummaryWTH, we can add time.windows
-#' W <- W.matrix(env.data = env.data, by.interval = TRUE,
+#' W <- W_matrix(env.data = env.data, by.interval = TRUE,
 #'               time.window = c(0, 14, 35, 60, 90, 120))
 #'
 #' ### Selecting statistic to be used
-#' W <- W.matrix(env.data = env.data, by.interval = TRUE, statistic = 'mean',
+#' W <- W_matrix(env.data = env.data, by.interval = TRUE, statistic = 'mean',
 #'               time.window = c(0, 14, 35, 60, 90, 120))
 #'
-#' W <- W.matrix(env.data = env.data, by.interval = TRUE, statistic = 'quantile',
+#' W <- W_matrix(env.data = env.data, by.interval = TRUE, statistic = 'quantile',
 #'               time.window = c(0, 14, 35, 60, 90, 120))
 #'
 #' ### With Quality Control (QC) based on the maximum sd tolerated
-#' W <- W.matrix(env.data = env.data, by.interval = FALSE, QC = TRUE)
+#' W <- W_matrix(env.data = env.data, by.interval = FALSE, QC = TRUE)
 #'
 #' ### With Quality Control (QC) based on the maximum sd tolerated
-#' W <- W.matrix(env.data = env.data, by.interval = FALSE, QC = TRUE, sd.tol = 3)
+#' W <- W_matrix(env.data = env.data, by.interval = FALSE, QC = TRUE, sd.tol = 3)
 #'
 #' ### Creating W for specific variables
 #' id.var = c('T2M_MAX','T2M_MIN','T2M')
-#' W <- W.matrix(env.data = env.data, var.id = id.var)
+#' W <- W_matrix(env.data = env.data, var.id = id.var)
 #'
 #' ### Combining summaryWTH by using is.processed = TRUE
 #' data <- summaryWTH(env.data, env.id = 'env', statistic = 'quantile')
-#' W <- W.matrix(env.data = data, is.processed = TRUE)
+#' W <- W_matrix(env.data = data, is.processed = TRUE)
 #'
 #' @importFrom stats sd
 #' @importFrom reshape2 acast
 #'
 #' @export
 
-W.matrix = function(env.data, is.processed=FALSE,id.names=NULL,env.id=NULL,var.id=NULL,
+W_matrix = function(env.data, is.processed=FALSE,id.names=NULL,env.id=NULL,var.id=NULL,
                     probs=NULL,by.interval=NULL,time.window=NULL,names.window=NULL,
                     center=TRUE,scale=TRUE, sd.tol = 10,statistic=NULL,
                     tol=1E-3, QC=FALSE){
-
+  
   if(is.null(statistic)) statistic <-'mean'
   if(is.null(by.interval)) by.interval <- FALSE
-
+  
   # if the env data are alredy processed in means, medians etc, ignore
   if(isFALSE(is.processed)){
     W<-summaryWTH(env.data=env.data,id.names=id.names,
@@ -81,20 +81,20 @@ W.matrix = function(env.data, is.processed=FALSE,id.names=NULL,env.id=NULL,var.i
                   by.interval=by.interval,
                   time.window=time.window,
                   names.window=names.window)
-
-
+    
+    
     colid <- c('env','variable','interval')
     W <- melt(W,measure.vars = names(W)[!names(W)%in%colid],variable.name = 'stat' )
     W$variable <-paste(W$variable,W$stat,sep = '_')
     if(isTRUE(by.interval)) W  <- reshape2::acast(W,env~variable+interval,value.var = "value")
     if(isFALSE(by.interval)) W <- reshape2::acast(W,env~variable,value.var = "value")
   }
-
+  
   # parametrization for w~N(0,1)
   W <- W.scale(env.data = W,center = center,scale = scale,sd.tol = sd.tol,QC = QC)
-
+  
   return(W)
-
+  
 }
 
 
@@ -102,16 +102,16 @@ W.matrix = function(env.data, is.processed=FALSE,id.names=NULL,env.id=NULL,var.i
 
 
 W.scale <-function(env.data, center=TRUE,scale=TRUE, sd.tol = 4,tol=1E-3,QC=FALSE){
-
+  
   sdA   <- apply(env.data,2,sd)
   t <- ncol(env.data)
   removed <- names(sdA[sdA > sd.tol])
   if(!is.matrix(env.data)){stop('env.data must be a matrix')}
   env.data<- scale(env.data+tol,center = center,scale = scale)
   if(isTRUE(QC)) env.data <- env.data[,!colnames(env.data) %in% removed]
-
+  
   r <- length(removed)
-
+  
   if(isTRUE(QC)){
     cat(paste0('------------------------------------------------','\n'))
     cat(paste0('Quality Control based on sd.tol=',sd.tol,'\n'))
@@ -120,6 +120,6 @@ W.scale <-function(env.data, center=TRUE,scale=TRUE, sd.tol = 4,tol=1E-3,QC=FALS
     cat(paste0(removed,'\n'))
     cat(paste0('------------------------------------------------','\n'))
   }
-
+  
   return(env.data)
 }
